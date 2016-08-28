58b460d20b1b68b62b7af4edf1aeba4546cade41
Nigel Small <nigel@nigelsmall.name>
2016-08-28 10:10:13 +0000

    Updated chunks server page

--- www/chunks.php
+++ www/chunks.php
@@ -2,14 +2,14 @@
 header('Content-Type: text/plain');
 $count = $_SERVER['QUERY_STRING'];
 if ($count == '') {
-	$count = 3;
+	$count = 5;
 }
 $count = (int) $count;
 flush();
 ob_flush();
 sleep(1);
 for ($i = 1; $i <= $count; $i++) {
-	print("chunk $i\r\n");
+	print(decbin($i));
 	flush();
 	ob_flush();
 	sleep(1);
